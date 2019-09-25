6dee8ad6b6bd674a7169fa05a7056fce5dee6355
asbillings07 <superswiftdeveloper@gmail.com>
2019-09-25 17:35:28 -0400

    add new routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { Home } from './components/Home';
+import { About } from './components/About';
 
 function App() {
   return (
     <Router>
       <Navigation />
       <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/about" />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
         <Router path="projects" />
         <Route path="/project/:id" />
       </Switch>
