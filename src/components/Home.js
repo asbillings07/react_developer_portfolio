4990baec7d1a4369e81e10e14bb3c952afb1f212
asbillings07 <superswiftdeveloper@gmail.com>
2019-11-11 18:06:03 -0500

    refactor home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { Projects } from './Projects';
-import { data } from '../data';
-import { Row } from 'react-bootstrap';
-import styled from 'styled-components';
+import React from "react";
+import { Projects } from "./Projects";
+import { ScrollView } from "./ScrollView";
+import { data } from "../data";
+import { Row, Container, Col } from "react-bootstrap";
+import styled from "styled-components";
 
 export function Home() {
   return (
-    <>
-      <h1>Welcome to my portfolio!</h1>
-      <p>
-        Here you will see a wide range of projects showcasing my software
-        development skills. All projects include by a github link to the code
-        and a live link where you can play with the app I build. Take a look
-        around, play with some of my projects. If you want to get to know me
-        better, you can checkout the about me section.{' '}
-      </p>
+    <Container>
+      <Row>
+        <Col>
+          <ScrollView projects={data.projects} />
+          <h1>Welcome to my portfolio!</h1>
+          <p>
+            Here you will see a wide range of projects showcasing my software
+            development skills. All projects include by a github link to the
+            code and a live link where you can play with the app I build. Take a
+            look around, play with some of my projects. If you want to get to
+            know me better, you can checkout the about me section.{" "}
+          </p>
 
-      <CardContainer>
-        <Row>
+          {/* <CardContainer>
           <Projects data={data} />
-        </Row>
-      </CardContainer>
-    </>
+        </CardContainer> */}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
