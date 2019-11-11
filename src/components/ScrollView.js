2a0451f4dd43d5d762bc6f26d676fae996db2603
asbillings07 <superswiftdeveloper@gmail.com>
2019-11-11 18:05:47 -0500

    add new scrollView component

---
+++ src/components/ScrollView.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+
+export function ScrollView({ projects }) {
+  return (
+    <Carousel pauseOnHover={true}>
+      {projects.map(project => (
+        <Carousel.Item>
+          <a href={`/project/${project.id}`}>
+            <img
+              className="d-block w-100"
+              src={project.image_urls[0]}
+              alt="Project"
+            />
+          </a>
+          <Carousel.Caption>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+}
