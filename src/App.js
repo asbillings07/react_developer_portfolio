d1aa816e5075e44c2ebd72b1b398c36fc97e1bee
asbillings07 <superswiftdeveloper@gmail.com>
2019-11-29 14:48:09 -0500

    added MUI to project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navigation } from './components/Navigation'
+import { AppNavBar } from './components/Navigation'
 import { Home } from './components/Home'
 import { About } from './components/About'
 import { ProjectDetail } from './components/ProjectDetail'
 import { Projects } from './components/Projects'
+import { Footer } from './components/Footer'
 
 function App () {
   return (
     <Router>
-      <Navigation />
+      <AppNavBar />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
         <Route path='/project/:id' component={ProjectDetail} />
         <Route path='/projects' component={Projects} />
       </Switch>
+      <Footer />
     </Router>
   )
 }
