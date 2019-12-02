8401ddf87878677eed864dc97cab147e7a1dbab5
asbillings07 <superswiftdeveloper@gmail.com>
2019-12-02 07:17:18 -0500

    updated grammar in about page

--- src/components/About.js
+++ src/components/About.js
@@ -49,12 +49,25 @@ export const About = () => {
             />
             <CardContent>
               <Typography variant='body2' color='textSecondary' component='p'>
-                Click any of the icons below to connect with me.
+                Click any of the icons below to connect with or learn more about
+                me. You can also checkout the articles I've written{' '}
+                <a
+                  href='https://medium.com/@asbillings2007'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  here
+                </a>
+                .
               </Typography>
             </CardContent>
             <CardActions disableSpacing>
               <IconButton aria-label='add to favorites'>
-                <Link href='https://github.com/asbillings07' target='_blank'>
+                <Link
+                  href='https://github.com/asbillings07'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
                   <GitHubIcon />
                 </Link>
               </IconButton>
@@ -62,7 +75,7 @@ export const About = () => {
                 <Link
                   href='https://www.linkedin.com/in/aaron-billings-9b429610a/'
                   target='_blank'
-                  rel='noopener'
+                  rel='noopener noreferrer'
                 >
                   <LinkedInIcon />
                 </Link>
@@ -71,7 +84,7 @@ export const About = () => {
                 <Link
                   href='https://twitter.com/abdevelops'
                   target='_blank'
-                  rel='noopener'
+                  rel='noopener noreferrer'
                 >
                   <TwitterIcon />
                 </Link>
@@ -94,39 +107,57 @@ export const About = () => {
           </Typography>
           <br />
           <Typography variant='h6' color='textSecondary'>
-            It's already been over year since I've been coding. Before I started
-            coding I was a support manager. I really enjoyed helping customers.
-            However, I wanted to be the one to actually solve the code behind
-            those customer issues. That curiousity lead me on a journey into the
-            world of Software development. I loved what I was learning so much
-            that I would spend most of my waking hours consuming information,
-            and working on projects.
+            It's already been over year since I started my coding journey.
+            Before I was a developer, I was a support manager. I really enjoyed
+            helping customers and helping my team grow.
           </Typography>
           <br />
           <Typography variant='h6' color='textSecondary'>
-            My passing for coding pushed me to code at least 8 hours a day while
-            working a full time 9-5 job. I would wake up everyday at 4:30am and
-            get to work by 5am. I would code until 9am, work my job until 12pm,
-            code during lunch, then work my job again until 5pm. After which I
-            would come home and code from 8pm until 12am.
+            However, I was always curious about how these issues were solved.
+            That curiousity lead me into the world of Software development. I
+            loved what I was learning so much that I would spend most of my
+            waking hours reading coding books, watching tutorials, and working
+            on projects.
           </Typography>
           <br />
           <Typography variant='h6' color='textSecondary'>
-            Being a developer is not a hobby for me, it's a career. One I hope
-            to pursue for the rest of my life. Coding makes me excited to get up
-            in the morning, and pushes me to learn new languages and
-            technologies. I am very passionate about software development and
-            want to encourage others that are on the same path.
+            When I join the Treehouse Techdegree program, my passion for
+            developing pushed me to code at least 8 hours a day while working a
+            full time 9-5 job. I would wake up everyday at 4:30am, get to work
+            by 5am, then code until 9am. I would work my full-time job until
+            12pm, code during lunch, then work my job again until 5pm. After
+            which I would come home and code from 8pm until 12am.
+          </Typography>
+          <br />
+          <Typography variant='h6' color='textSecondary'>
+            I'm still amazed that I able to purse my passion and make a living.
+            I never would have thought that was possible years ago. Coding makes
+            me excited to get up in the morning, and pushes me to learn new
+            languages and technologies. I am very passionate about software
+            development and enjoy encouraging others that are on the same path.
           </Typography>
           <br />
           <Typography variant='h6' color='textSecondary'>
             When I'm not coding, I enjoy spending time with my wife and two
             daughters, playing video games, and watching movies.
           </Typography>
+          <br />
           <Typography variant='h6' color='textSecondary'>
-            Thanks for checking out my portfolio, if you like what you've seen
-            and heard reach out, I'd love to hear from you. I'm always on the
-            lookout for my next great adventure.
+            Thanks for checking out my portfolio! If you like what you seen, I'd
+            love to hear from you. I'm always on the lookout for my next great
+            adventure.
+          </Typography>
+          <br />
+          <Typography variant='h6' color='textSecondary'>
+            Also, checkout some of the articles I've written{' '}
+            <a
+              href='https://medium.com/@asbillings2007'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              here
+            </a>
+            .
           </Typography>
         </Col>
       </Row>
